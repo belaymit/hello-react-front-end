a95a15e14eb14f492d3403c3d7cd4cbf48f16e0d
belaymit <2belamit@gmail.com>
2023-05-02 14:56:05 +0300

    Added provider and browser-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import { fetchGreeting } from './redux/reducer/greetings';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+store.dispatch(fetchGreeting());
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
