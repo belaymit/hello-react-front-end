c4cf85de7b26cd485f3dc99b7e1cc7cefd3d36bc
belaymit <2belamit@gmail.com>
2023-05-02 15:01:34 +0300

    Resolved path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-import { fetchGreeting } from './redux/reducer/greetings';
+import { fetchGreeting } from './redux/greetingSlice/greetings';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
